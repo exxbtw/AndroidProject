5fef49249634203e6410f11aa0e53c7cb67017a8
exxbtw <kuzayur@gmail.com>
2025-09-25 16:35:20 +0700

    Создание интерфейса и разделение на файлы

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,17 @@
-class Human(val FIO: String, var age: Int, var speed: Double) {
-    var x: Double = 0.0
-    var y: Double = 0.0
+import kotlinx.coroutines.*
 
-    fun move() {
-        var dx = (-1..1).random()
-        var dy = (-1..1).random()
-        x = x + dx * speed
-        y = y + dy * speed
-    }
-}
-
-fun main() {
-    val humans = Array(14) { i ->
+fun main() = runBlocking {
+    val humans = List(4) { i ->
         Human("Human${i + 1}", 18 + i, 2.0 + i * 0.2)
     }
 
-    val simulationTime = 10
-
-    for (t in 1..simulationTime) {
-        println("$t-ая секунда")
-        for (h in humans) {
-            h.move()
-            println("${h.FIO} - (${h.x}, ${h.y})")
-        }
+    val drivers = List(1) { i ->
+        Driver("Driver${i + 1}", 19 + i, 19.0 + i * 0.2, "Hyundai Solaris ${i + 1}")
     }
+
+    val jobs = (humans + drivers).map { obj ->
+        launch { obj.run() }
+    }
+    jobs.forEach { it.join() }
 
 }
